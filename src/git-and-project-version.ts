baf17bc85737cb803f924f3a6b2838b0820cdf61
Lieene Guo <lieene@shaderealm.com>
2019-12-09 16:12:14 +0800

    removed unused source file

--- src/git-and-project-version.ts
+++
@@ -1,8 +0,0 @@
-// File: git-and-project-version.ts                                                //
-// Project: lieene.UnityHelper                                                     //
-// Author: Lieene Guo                                                              //
-// MIT License, Copyright (c) 2019 Lieene@ShadeRealm                               //
-// Created Date: Thu Dec 5 2019                                                    //
-// Last Modified: Thu Dec 05 2019                                                  //
-// Modified By: Lieene Guo                                                         //
-
